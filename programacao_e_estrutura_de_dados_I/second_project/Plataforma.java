ee03116b8e2a18623493f985db21ed4f7a43bbfe
Augusto Alves da Silva <augustoalvess@hotmail.com>
2017-10-17 22:02:59 -0200

    Trabalhando no segundo projeto

--- programacao_e_estrutura_de_dados_I/second_project/Plataforma.java
+++ programacao_e_estrutura_de_dados_I/second_project/Plataforma.java
@@ -14,16 +14,32 @@ public class Plataforma {
 
 
 
-            
+
 
             while (bufferedReader.ready())
             {
                 String linha = bufferedReader.readLine();
                 String[] linhaSplit = linha.split(";");
 
+                if (plataformas.containsKey(linhaSplit[4])) {
+                	plataformas.get(linhaSplit[4]).put(
+                		"número de reviews", 
+                		(Integer.parseInt(plataformas.get(linhaSplit[4]).get("número de reviews").toString()) + 1)
+                	);
+
+                	if (linhaSplit[7].toString().equals("Y")) {
+                		plataformas.get(linhaSplit[4]).put(
+	                		"número de Editor’s Choice", 
+	                		(Integer.parseInt(plataformas.get(linhaSplit[4]).get("número de Editor’s Choice").toString()) + 1)
+	                	);                		
+                	}
+
+                	continue;
+                }
+
 				HashMap<String, Integer> valores = new HashMap<String, Integer>();
-				valores.put("número de reviews", 0);
-				valores.put("número de Editor’s Choice", 0);
+				valores.put("número de reviews", 1);
+				valores.put("número de Editor’s Choice", ((linhaSplit[7] == "Y") ? 1 : 0));
 				valores.put("percentual de ‘Masterpiece’ ou ‘Amazing’ reviews", 0);
 				valores.put("média de todos os scores", 0);
 				valores.put("desvio padrão (para variável aleatória discreta) de todos os scores", 0);
