c27467ccbc1a6d6f50c1ecb4b5e3bceca18d7374
Augusto Alves da Silva <augustoalvess@hotmail.com>
2018-03-19 19:54:16 -0300

    Corrigidos problemas de timeout

--- programacao_paralela_e_distribuida/trabalho01/MFTPServer.java
+++ programacao_paralela_e_distribuida/trabalho01/MFTPServer.java
@@ -10,24 +10,26 @@ import java.net.InetAddress;
 public class MFTPServer {
 
     public static void main(String[] args) throws Exception {
-        FileInputStream file = new FileInputStream("/home/augusto/Downloads/imagem.jpg");
+        FileInputStream file = new FileInputStream("/home/augusto.silva/Downloads/imagem.jpg");
         DatagramSocket serverSocket = new DatagramSocket();
         serverSocket.setSoTimeout(5);
 
         byte[] buffer = new byte[1024];
         int bytes = 0;
+	int id = 1;
         while ((bytes = file.read(buffer)) != -1) {
-            Mensagem m = new Mensagem(buffer, bytes);
-            MFTPServer.enviarMensagem(serverSocket, m, true);
+            Mensagem m = new Mensagem(id, buffer, bytes);
+            MFTPServer.enviarMensagem(serverSocket, m);
+	    id++;
         }
-        Mensagem m = new Mensagem(buffer, -1);
-        MFTPServer.enviarMensagem(serverSocket, m, true);
+        Mensagem m = new Mensagem(id, buffer, -1);
+        MFTPServer.enviarMensagem(serverSocket, m);
 
         file.close();
         serverSocket.close();
     }
 
-    public static void enviarMensagem(DatagramSocket serverSocket, Mensagem m, boolean retransmitir) {
+    public static void enviarMensagem(DatagramSocket serverSocket, Mensagem m) {
         byte[] sendData = new byte[2048];
         byte[] receiveData = new byte[1024];
 
@@ -38,29 +40,18 @@ public class MFTPServer {
             streamMensagem.close();
             sendData = streamBytes.toByteArray();
             
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("230.0.0.1"), 9876);
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("230.37.40.22"), 9876);
             serverSocket.send(sendPacket);
 
-            // Política Stop-and-Wait
-            while (true) {
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+	    for (int x = 1; x <= 1; x ++)  {
+		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 serverSocket.receive(receivePacket);
                 String receive = new String(receivePacket.getData());
-                System.out.println("Pacote de " + receive.length() + " bytes enviados para o client " + receivePacket.getAddress().getHostAddress());
-                break;
-            }
+                System.out.println("Pacote de " + receive.length() + " bytes enviados para o client " + receivePacket.getAddress().getHostAddress());	         
+	    }
         } catch (InterruptedIOException e) {
-            System.out.print("Tempo de transmissão excedido");
-            if (e.getMessage() != "Receive timed out") { // Caso a resposta do cliente tenha excedido o timeout, somente notifica.
-                System.out.println("..." + e.getMessage());
-            } else { // Caso tenha sito o envio que tenha ocasionado timeout, executa o reenvio do pacote.
-                if (retransmitir) {
-                    System.out.println("...reenviando pacote para o client");
-                    MFTPServer.enviarMensagem(serverSocket, m, false);
-                } else {
-                    System.out.println();
-                }
-            }
+            System.out.println("Tempo de execução excedido...reenviando pacote para o client");
+            MFTPServer.enviarMensagem(serverSocket, m);
         }catch(IOException ioe){
             System.out.println("Falha na transmissão de rede");
         }
