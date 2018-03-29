cd3f206aae53c133c2b8d84e4b7d0318e95d30b0
Augusto Alves da Silva <augustoalvess@hotmail.com>
2018-03-29 19:43:29 -0300

    Implementando procedures do trabalho

--- sistemas_de_banco_de_dados/procedures.sql
+++ sistemas_de_banco_de_dados/procedures.sql
@@ -10,11 +10,54 @@ BEGIN
     SET qtd_disponivel = FLOOR(RAND() * 100);      
         
     REPEAT
-	INSERT INTO livro VALUES(id, editora_id, 'LIVRO DE TESTE ' + @x, edicao, data_publicacao, local_publicacao, qtd_total, qtd_disponivel);
+	INSERT INTO livro VALUES(id, editora_id, concat('LIVRO DE TESTE ', @x), edicao, data_publicacao, local_publicacao, qtd_total, qtd_disponivel);
         SET @x = @x + 1;
     UNTIL @x > pcodemax
     END REPEAT;
  
     SELECT pcodeini;
 END$$
-DELIMITER ;
+DELIMITER;
+
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS insereusuario$$
+CREATE PROCEDURE insereusuario (pcodeini INT, pcodemax INT)
+BEGIN
+    SET @x = pcodeini;
+        
+    REPEAT
+    INSERT INTO usuario VALUES (id, concat('USUÁRIO TESTE ', @x), concat('teste', @x, '@teste.com'), concat('user', @x), concat('user', @x), 1);
+        SET @x = @x + 1;
+    UNTIL @x > pcodemax
+    END REPEAT;
+ 
+    SELECT pcodeini;
+END$$
+DELIMITER;
+
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS insereretirada$$
+CREATE PROCEDURE insereretirada (pcodeini INT, pcodemax INT)
+BEGIN
+    DECLARE atendente_id INT;
+    DECLARE usuario_retirante_id INT;
+    DECLARE situacao_id INT;
+    DECLARE config_de_multa_id INT;
+
+    SET @x = pcodeini;
+    SET atendente_id = FLOOR(1 + (RAND() * 40));
+    SET usuario_retirante_id = FLOOR(1 + (RAND() * 500000));
+    SET situacao_id = FLOOR(1 + (RAND() * 200));
+    SET config_de_multa_id = FLOOR(1 + (RAND() * 200));
+        
+    REPEAT
+    INSERT INTO retirada VALUES(id, atendente_id, usuario_retirante_id, situacao_id, config_de_multa_id, (CURDATE() - INTERVAL FLOOR(RAND() * 50000) DAY), (CURDATE() - INTERVAL FLOOR(RAND() * 50000) DAY), (CURDATE() - INTERVAL FLOOR(RAND() * 50000) DAY), FLOOR(RAND() * 10));
+        SET @x = @x + 1;
+    UNTIL @x > pcodemax
+    END REPEAT;
+ 
+    SELECT pcodeini;
+END$$
+DELIMITER;
