5b32478839ac793652af5da4106e7379a613925f
Augusto Alves da Silva <augustoalvess@hotmail.com>
2018-04-11 13:39:30 -0300

    Ajustando

--- programacao_e_estrutura_de_dados_II/FirstProject.java
+++ programacao_e_estrutura_de_dados_II/FirstProject.java
@@ -3,8 +3,8 @@ public class FirstProject {
 		// Resultado de um percurso em pré-ordem, portanto o inverso da pré-ordem deverá
 		// reconstruir a árvore.
 
-		BinaryTree tree = makeBinaryTreeByExp("* + 3 6 - 4 1");
-		//BinaryTree tree = makeBinaryTreeByExp("+ * + 3 6 - 4 1 5");
+		//BinaryTree tree = makeBinaryTreeByExp("* + 3 6 - 4 1");
+		BinaryTree tree = makeBinaryTreeByExp("+ * + 3 6 - 4 1 5");
 		//BinaryTree tree = makeBinaryTreeByExp("+ 6 5");
 		preorder(tree.getRoot());
 	}
@@ -12,47 +12,36 @@ public class FirstProject {
 	public static BinaryTree makeBinaryTreeByExp(String exp) {		
 		String expElements[] = exp.split(" ");
 		BinaryTree tree = new BinaryTree(expElements[0]);
-		Node n;
+		Node after = tree.getRoot();
 
 		for (int x = 1; x < expElements.length; x++) {
-			n = getFatherNode(tree.getRoot(), expElements[x]);
-			if (n.left == null) {
-				tree.addLeftChild(n, expElements[x]);
+			while (after.left != null && after.right != null) {
+				after = after.parent;
+			}
+
+			if (after.left == null) {
+				tree.addLeftChild(after, expElements[x]);
+				if (!isNumber(expElements[x])) {
+					after = after.left;
+				}
 			} else {
-				tree.addRightChild(n, expElements[x]);
+				tree.addRightChild(after, expElements[x]);
+				if (!isNumber(expElements[x])) {
+					after = after.right;
+				}
 			}
 		}
 
 		return tree;
 	}
 
-	public static Node getFatherNode(Node n, String s) {
-		Node left, right;
-
-		if (n.left != null) {
-			left = getFatherNode(n.left, s);
-			System.out.println("left" + left);
-		}
-
-		if (n.right != null) {
-			right = getFatherNode(n.right, s);
-			System.out.println("right" + right);
-		}
-
-
-		
-		
-
-		return n;
-	}
-
 	public static void preorder(Node n) {
 	    // 1. node
 	    System.out.println(n.element);
 	    // 2. left child
-	    if (n.left != null) { System.out.print("left "); preorder(n.left); }
+	    if (n.left != null) { System.out.print("parent " + n.element + ", left "); preorder(n.left); }
 	    // 3. right child
-	    if (n.right != null) { System.out.print("right "); preorder(n.right); }
+	    if (n.right != null) { System.out.print("parent " + n.element + ", right "); preorder(n.right); }
     }
 
     public static boolean isNumber(String s) {
