12349852aaeabe8bdec6ee7f89c07e2fa53175e7
Augusto Alves da Silva <augustoalvess@hotmail.com>
2018-05-19 12:38:26 -0300

    Implementado segundo projeto de pedII

--- programacao_e_estrutura_de_dados_II/second_project/Tarefa1Solver.java
+++ programacao_e_estrutura_de_dados_II/second_project/Tarefa1Solver.java
@@ -3,23 +3,18 @@ public class Tarefa1Solver {
     public static int count;
 
     public static void main(String[] args) {
-        solve(new Tarefa1State(2, 7));
+        int[][] bloqueados = new int[][]{{0, 5}, {1, 4}};
+        solve(new Tarefa1State(2, 7, bloqueados));
+        System.out.println("count: " + count);
     }
     
     public static void solve(Tarefa1State state) {
-    	/**
     	if (state.isSolution()) {
-
+            System.out.println(state);
+            count++;
     	} else {
-    		solve(new Tarefa1State("a"));
-            solve(new Tarefa1State("b"));
-            solve(new Tarefa1State("c"));
-            solve(new Tarefa1State("d"));
+    	   solve(new Tarefa1State(state, new char[]{'a', 'b', 'c', 'd'}));
     	}
-    	*/
-
-
-        System.out.println(state);
     }
 }
 
--- programacao_e_estrutura_de_dados_II/second_project/Tarefa1State.java
+++ programacao_e_estrutura_de_dados_II/second_project/Tarefa1State.java
@@ -1,33 +1,111 @@
 public class Tarefa1State {
-	public int l;
-	public int c;
+
 	public char[][] caixa;
 
-	public Tarefa1State(int l, int c) {
-		this.l = l;
-		this.c = c;
+	public Tarefa1State(int l, int c, int[][] bloqueados) {
 		this.caixa = new char[l][c];
+		for (int x = 0; x < bloqueados.length; x++) {
+			this.caixa[bloqueados[x][0]][bloqueados[x][1]] = 'x';
+		}
 	}
 
-	public Tarefa1State(char e) {
-		// Aqui preciso fazer a lógica de inserção em L do elemento na mátriz,
-		// Somente se o elemento cabe nos espaços da matriz, ou seja, se não
-		// existe elementos nos espaços necessários para montar um L.
+	public Tarefa1State(Tarefa1State s, char[] pecas) {
+		this.caixa = s.caixa;
+		for (char e: pecas) {
+			int[][][] bloco = this.obterBlocoLivre(e);
+			if (bloco != null) {
+				if (e == 'a') {
+					this.caixa[bloco[0][0][0]][bloco[0][0][1]] = e;
+					this.caixa[bloco[1][0][0]][bloco[1][0][1]] = e;
+					this.caixa[bloco[1][1][0]][bloco[1][1][1]] = e;
+				}
+				if (e == 'b') {
+					this.caixa[bloco[0][0][0]][bloco[0][0][1]] = e;
+					this.caixa[bloco[0][1][0]][bloco[0][1][1]] = e;
+					this.caixa[bloco[1][1][0]][bloco[1][1][1]] = e;
+				}
+				if (e == 'c') {
+					this.caixa[bloco[0][0][0]][bloco[0][0][1]] = e;
+					this.caixa[bloco[0][1][0]][bloco[0][1][1]] = e;
+					this.caixa[bloco[1][0][0]][bloco[1][0][1]] = e;
+				}
+				if (e == 'd') {
+					this.caixa[bloco[0][1][0]][bloco[0][1][1]] = e;
+					this.caixa[bloco[1][0][0]][bloco[1][0][1]] = e;
+					this.caixa[bloco[1][1][0]][bloco[1][1][1]] = e;
+				}
+			}
+		}
+	}
+
+
+	public int[][][] obterBlocoLivre(char e) {
+		int[][][] bloco = new int[2][2][2];
+		for (int l = 0; l < this.caixa.length; l++) {
+			for (int c = 0; c < this.caixa[0].length; c++) {
+				if (l+1 < this.caixa.length && c+1 < this.caixa[0].length) {
+					// cada elemento possui uma orientação diferente:
+					// a=a  b=bb c=cc d= d
+					//   aa    b   c    dd
+					if (e == 'a') {
+						if (this.caixa[l][c] == 0 && this.caixa[l+1][c] == 0 && this.caixa[l+1][c+1] == 0) {
+							bloco[0][0] = new int[]{l, c};
+							bloco[1][0] = new int[]{l+1, c};
+							bloco[1][1] = new int[]{l+1, c+1};
+							return bloco;
+						}
+					}
+					if (e == 'b') {
+						if (this.caixa[l][c] == 0 && this.caixa[l][c+1] == 0 && this.caixa[l+1][c+1] == 0) {
+							bloco[0][0] = new int[]{l, c};
+							bloco[0][1] = new int[]{l, c+1};
+							bloco[1][1] = new int[]{l+1, c+1};
+							return bloco;
+						}
+					}
+					if (e == 'c') {
+						if (this.caixa[l][c] == 0 && this.caixa[l][c+1] == 0 && this.caixa[l+1][c] == 0) {
+							bloco[0][0] = new int[]{l, c};
+							bloco[0][1] = new int[]{l, c+1};
+							bloco[1][0] = new int[]{l+1, c};
+							return bloco;
+						}
+					}
+					if (e == 'd') {
+						if (this.caixa[l][c+1] == 0 && this.caixa[l+1][c] == 0 && this.caixa[l+1][c+1] == 0) {
+							bloco[0][1] = new int[]{l, c+1};
+							bloco[1][0] = new int[]{l+1, c};
+							bloco[1][1] = new int[]{l+1, c+1};
+							return bloco;
+						}
+					}
+				}
+			}
+		}
+		return null;
 	}
 
 	public boolean isSolution() {
-
-		// Medir os espaços vazios, verificar se não cabe mais nenhuma peça, então é solução.
-
-        return true;
+		if (obterBlocoLivre('a') == null && obterBlocoLivre('b') == null && obterBlocoLivre('c') == null && obterBlocoLivre('d') == null) {
+			return true;
+		}
+		for (int l = 0; l < this.caixa.length; l++) {
+			for (int c = 0; c < this.caixa[0].length; c++) {
+				if (this.caixa[l][c] == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
     }
 
 	public String toString() {
 		String resultado = "";
-		for (int x = 0; x < l; x++) {
-			for (int y = 0; y < c; y++) {
-				if (caixa[x][y] != 0) {
-					resultado += " " + caixa[x][y] + " ";
+		for (int l = 0; l < this.caixa.length; l++) {
+			for (int c = 0; c < this.caixa[0].length; c++) {
+				if (caixa[l][c] != 0) {
+					resultado += " " + caixa[l][c] + " ";
+					continue;
 				}
 				resultado += " - ";
 			}
