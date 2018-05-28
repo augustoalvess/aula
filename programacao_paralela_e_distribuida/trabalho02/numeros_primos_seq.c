c95c445fe5ae46aaa7fb259b1402678c9d56eaee
Augusto Alves da Silva <augustoalvess@hotmail.com>
2018-05-28 19:51:15 -0300

    Ajustado

--- programacao_paralela_e_distribuida/trabalho02/numeros_primos_seq.c
+++ programacao_paralela_e_distribuida/trabalho02/numeros_primos_seq.c
@@ -2,21 +2,22 @@
 #include <string.h>
 
 void main() {
-	int tamanhoVetor = 2500;
+	int tamanhoVetor = 500000;
 	int resultado = 0;
 
 	for (int x = 1; x <= tamanhoVetor; x=x+2) {
 		int cont = 0;
 
 		// verificar se i é um número primo.
-		for (int i = 1; i <= x; i++) {
+		for (int i = 3; i <= x; i++) {
     		if (x % i == 0) {
      			cont++;
+     			break;
     		}
     	}
     
     	// se for primo, soma resultado + 1
-  		if (cont == 2) {
+  		if (cont == 1) {
   			resultado++;
   		} 		
 	}
