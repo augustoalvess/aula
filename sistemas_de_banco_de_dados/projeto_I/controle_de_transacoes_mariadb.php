efe3dd9018b925b10bd617a5e7d026505c36fdde
Augusto Alves da Silva <augustoalvess@hotmail.com>
2018-06-07 19:44:07 -0300

    Ajustado

--- sistemas_de_banco_de_dados/projeto_I/controle_de_transacoes_mariadb.php
+++ sistemas_de_banco_de_dados/projeto_I/controle_de_transacoes_mariadb.php
@@ -6,7 +6,7 @@ $conn = new PDO('mysql:host=localhost;dbname=biblioteca_sbd', 'root');
 $conn->beginTransaction();
 try {
 	$ok = $conn->exec("UPDATE livro SET nome = 'TESTE' WHERE id = 1");
-	//$ok = $conn->exec("UPDATE livro SET id = 'TESTE' WHERE id = 1");
+	$ok = $conn->exec("UPDATE livro SET id = 'TESTE' WHERE id = 1");
 	if (!$ok) {
 		throw new Exception("Erro\n");
 	}
